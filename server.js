4dab595994aebbefc2527a910451ddcb2af62f97
Abdul Mohamed <abdulmohamed@Abduls-MacBook-Pro.local>
2026-08-27 13:38:06 +0530

    Update backend and CORS configuration

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 5001;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
+     
       "https://sanurpickle.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
